1b583323d982e9dc2426c5988fa45a0675796214
favless <122537119+favless@users.noreply.github.com>
2025-04-05 19:56:20 +0300

    Task button upgrade and dropdown transition

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const filterDropdown = document.getElementById("filter-dropdown")
 const dropdownArrow = document.getElementById("dropdown-arrow")
 
+const filterHeight = filterDropdown.offsetHeight
+console.log(filterHeight)
+filterDropdown.style.height = "0px";
+
 function toggleFilterDropdown() {
-    if (filterDropdown.style.display == "none") {
-        filterDropdown.style.display = "flex"
+    if (filterDropdown.style.height == "0px") {
+        filterDropdown.style.height = filterHeight + "px"
         dropdownArrow.style.rotate = "0deg"
     } else {
-        filterDropdown.style.display = "none"
+        filterDropdown.style.height = "0px"
         dropdownArrow.style.rotate = "180deg"
     }
 }
