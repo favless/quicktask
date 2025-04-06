a5fca89bded5d937589c96443925a68b5fa484e3
favless <122537119+favless@users.noreply.github.com>
2025-04-06 12:58:11 +0300

    Added animations for most elements

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 const filterDropdown = document.getElementById("filter-dropdown")
 const dropdownArrow = document.getElementById("dropdown-arrow")
+const addButton = document.querySelector(".add-button");
+const addButtonLabel = document.getElementById("add-button-label")
 
 const filterHeight = filterDropdown.offsetHeight
-console.log(filterHeight)
 filterDropdown.style.height = "0px";
+
+const addButtonLabelWidth = addButtonLabel.offsetWidth
+addButtonLabel.style.maxWidth = "0px"
+
+
+addButton.addEventListener("mouseenter", () => {
+    addButtonLabel.style.maxWidth = addButtonLabelWidth + "px"
+})
+
+addButton.addEventListener("mouseleave", () => {
+    addButtonLabel.style.maxWidth = "0px"
+})
 
 function toggleFilterDropdown() {
     if (filterDropdown.style.height == "0px") {
